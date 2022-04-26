48dba585c2c5e2e153408ab8ee72032247cbcdfe
LigiaBSilva <ligiaborgessilva@hotmail.com>
2022-04-26 13:34:30 +0100

    commiting my work

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+
+    
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal_amount = document.getElementById("principal").value;
+   
+   var rate = document.getElementById("rate").value;
+
+   var years = document.getElementById("years").value;
+
+   var interest = principal_amount*rate*years/100;
+
+   var year= new Date().getFullYear()+parseInt(years)
+   
+    if (principal_amount == "")
+        {alert ("please enter a positive number");
+        principal.focus();
+        return false;}
+
+    if (principal_amount <=0)
+    {alert ("please enter a positive number");
+    principal.focus();
+    return false;}
+
+    else  
+    text_amount.innerText = principal_amount;
+    text_rate.innerText = rate;
+    profit.innerText = interest;
+    text_years.innerText = year;
+    return true;}
+
+function slider_value(){
+    var rate = document.getElementById("rate");
+    var ratedisplay = document.getElementById("rate_val");
+    rate_val.innerText = rate.value+"%";
 }
-        
+function ligia_function(){
+ligia.innerText = "hoje"}
